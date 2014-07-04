361c716e0cec0cc8cafa35f15a496994423f62a2
Mariyan <mtashev@Mariyans-Mac.local>
2014-07-04 13:02:59 +0300

    Initial Commit

---
+++ FineWine/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FineWine
+//
+//  Created by Mariyan on 7/4/14.
+//  Copyright (c) 2014 Mariyan Tashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FineWine/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FineWine
+//
+//  Created by Mariyan on 7/4/14.
+//  Copyright (c) 2014 Mariyan Tashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
